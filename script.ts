b750caea5128fcb111ff0996bcc79b2cf22ae671
Prashant <prashantmahamuni9803@gmail.com>
2024-07-30 11:55:36 +0530

    any data type error

--- script.ts
+++ script.ts
@@ -1,1 +1,9 @@
-var a = 10;
+// types: 
+
+//1) Primitives: number bool undefined string char
+//2) Reference: object array function tuples
+
+
+//if we declare a var, ts should know what type it is
+let a : number; 
+
